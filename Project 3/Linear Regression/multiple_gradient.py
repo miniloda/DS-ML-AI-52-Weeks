cb664a1a6fe75f8f1c51d31e69f2aef6c8a71b67
Jacob Maximus <jlusaraga@up.edu.ph>
2024-07-19 08:23:06 +0800

    FIx Linear Regression

--- Project 3/Linear Regression/multiple_gradient.py
+++ Project 3/Linear Regression/multiple_gradient.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class LinearRegression:
 
-    def __init__(self, max_iter=100000):
+    def __init__(self, max_iter=1000):
         self.w_arr = None
         self.b = None
         self.J_arr = None
@@ -13,39 +13,48 @@ class LinearRegression:
         self.w_arr, self.b, self.J_arr = gradient_descent(X, y, self.max_iter)
 
     def predict(self, X):
-        return np.dot(X, self.w_arr) + self.b
+        X = X.reshape(-1, 1) if len(X.shape) == 1 else X
+        return np.dot(X, self.w_arr.T) + self.b
 
 
 def abs_distance(w_old, w_new):
     return np.linalg.norm(w_new - w_old)
 
 def scale(X):
-    for i in range(X.shape[1]):
-        X[:, i] = (X[:, i] - np.mean(X[:, i])) / np.std(X[:, i])
-    return X
-def gradient_descent(X, y, max_iter, b=0, a=0.1, err=0.001):
-    
+    """
+    Scales the input features.
+
+    Parameters:
+    X (numpy.ndarray): Input features of shape (m, n), where m is the number of samples and n is the number of features.
+
+    Returns:
+    numpy.ndarray: Scaled input features.
+    """
+    return (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+def gradient_descent(X, y, max_iter, b=0, a=0.1, err=0.0001):
+    X = X.reshape(-1, 1) if len(X.shape) == 1 else X
     m,n = X.shape
     # Initialize weights
     w_arr_old = np.random.randn(n) * 0.1
     w_arr = np.zeros(X.shape[1], dtype=np.float64)
     J_arr = np.zeros(max_iter + 1)
     X = scale(X)
+    print(X)
     y = np.array(y, dtype=float)
     b_old = b
     for i in range(max_iter):
         J_arr[i] = compute_cost(X, y, w_arr, b, i)
-        dj_dw, dj_db = compute_gradient(X, y, w_arr, b, J_arr, a, i)
+        dj_dw, dj_db = compute_gradient(X, y, w_arr, b, a, i)
 
-        w_arr[:] = w_arr_old[:] - a * dj_dw
+        w_arr[:] = w_arr_old[:] - np.dot(a,dj_dw)
         b = b_old - a * dj_db
         # assign the current weights to old weights for the next iteration
         w_arr_old = w_arr.copy()
         b_old = b
-        # Check if the weights have converged
-        #if abs_distance(w_arr, w_arr) <= err and abs(b - b_old) <= err:
-         #   print("Converged at iteration", i)
-          #  return w_arr, b
+         #Check if the weights have converged
+        #if abs_distance(J_arr[i], J_arr[i-1]) <= err:
+        #    print("Converged at iteration", i)
+        #    return w_arr, b
 
     print("Did not converge after", max_iter, "iterations", ". Try increasing max_iter.")
     return w_arr, b, J_arr
@@ -69,7 +78,7 @@ def compute_cost(X, y, w, b, i):
     return np.sum((np.dot(X, w) + b - y) ** 2) / (2 * m)
 
 
-def compute_gradient(X, y, w_arr, b, J_arr, alpha, i):
+def compute_gradient(X, y, w_arr, b, alpha, i):
     """
        Computes the gradient of the cost function for linear regression.
 
@@ -86,6 +95,6 @@ def compute_gradient(X, y, w_arr, b, J_arr, alpha, i):
        tuple: A tuple containing the gradient of weights (dj_dw) and the gradient of bias (dj_db).
        """
     m = X.shape[0]
-    dj_dw = np.array([np.sum([(w_arr[j]*X[i,j] - y[i])*X[i,j] for i in range(m)]) / m for j in range(X.shape[1])], dtype = np.float64)
-    dj_db = np.sum([(b*X[i,j] - y[i])*X[i,j] for i in range(m) for j in range(X.shape[1])]) / m
+    dj_dw = np.dot(np.dot(X, w_arr) + b * np.ones(m) - y, X) / m
+    dj_db = np.sum(np.dot(X, w_arr) + b *np.ones(m) - y) / m
     return dj_dw, dj_db
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Data-Science-52-Weeks
 In this repository, I will attempt to do around 30 projects centered around Data Science, Machine Learning, and AI in 52 weeks. This is to challenge myself to build data science projects, convert them into a data science portfolio, and learn along the way.
 This can span into data cleaning, visualization, machine learning, etc.
-You can fork this repository to draw inspiration or continue my footsteps.
+You can fork this repository to draw inspiration or continue my footsteps. <br><br>
+[![wakatime](https://wakatime.com/badge/user/f5331c0b-eb4e-443c-b006-aaadd1d3f780/project/3dc5e4dd-1f10-4c46-a48e-bb189c25bef2.svg)](https://wakatime.com/badge/user/f5331c0b-eb4e-443c-b006-aaadd1d3f780/project/3dc5e4dd-1f10-4c46-a48e-bb189c25bef2)
 
-
-|Project| Name    | Description| Link|  Status  |
-|-------|---------|---------|--------|----------|
-|   1   |         |         |        | Done  |
-|2      |         |         |        | Ongoing  |
-|   3   |           
-|   4   |         | Making a Flask App with a NN backend API|[Project 4]('https://github.com/miniloda/DS-ML-AI-52-Weeks/blob/main/Project%204') | Ongoing|
-|   5   | Scratch NN| Making a Neural Network from scratch| 
+| Project | Name       | Description                                          | Link                                                                               | Status      |
+|---------|------------|------------------------------------------------------|------------------------------------------------------------------------------------|-------------|
+| 1       |            |                                                      |                                                                                    | Done        |
+| 2       |            |                                                      |                                                                                    | Ongoing     |
+| 3       |            | Implementing linreg and logreg with Gradient Descent |                                                                                    | Ongoing     |
+| 4       |            | Making a Flask App with a NN backend API             | [Project 4]('https://github.com/miniloda/DS-ML-AI-52-Weeks/blob/main/Project%204') | Ongoing     |
+| 5       | Scratch NN | Making a Neural Network from scratch                 |                                                                                    | Not Started |
